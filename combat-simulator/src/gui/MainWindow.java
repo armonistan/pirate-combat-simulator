88a3bc3c8afda81674de4fb819544b4afff4a579
armonistan <armonistan@gmail.com>
2018-01-29 11:06:39 -0600

    can now use gui to simulate basic hard coded 1v1s for sims 1 and 2

--- combat-simulator/src/gui/MainWindow.java
+++ combat-simulator/src/gui/MainWindow.java
@@ -1,28 +1,82 @@
 package gui;
 
-import java.awt.Button;
-import java.awt.Label;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-import combatSimulations.CombatSimulationOne;
-import combatSimulations.CombatSimulationResultsStruct;
+import combatSimulations.*;
 
 public class MainWindow extends JFrame{
-	private ArrayList<Button> buttons;
-	private ArrayList<Label> labels;
+	private ArrayList<JButton> buttons;
+	private ArrayList<JLabel> labels;
 	
 	public MainWindow(){
 		setSize(500,500);
-		setTitle("Pirates Combat Sim 0.0.1");
+		setTitle("Pirates Combat Sim 0.0.2");
 		setVisible(true);
-		loadCombatSimulationOne();
+		setLayout(new FlowLayout());
+		setDefaultCloseOperation( EXIT_ON_CLOSE );
+		buttons = new ArrayList<JButton>();
+		labels = new ArrayList<JLabel>();
+		loadMainMenu();
 	}
 	
 	private void loadCombatSimulationOne(){	
 		CombatSimulationResultsStruct data = CombatSimulationOne.oneVersusOne(100, 3, 3, 3);
 		
 		System.out.println(data.numberOfAttackerWins + " " + data.numberOfDefenderWins + " " + data.numberOfDefenderDodges);
+	}
+	
+	private void loadCombatSimulationTwo(){
+		CombatSimulationResultsStruct data = CombatSimulatorTwo.oneVersusOne(100, 3, 3);
+		
+		System.out.println(data.numberOfAttackerWins + " " + data.numberOfDefenderWins);
+	}
+	
+	private void loadMainMenu(){
+		//sim one
+		JButton loadSimulationOneButton = new JButton("Load Simulation One");	
+		loadSimulationOneButton.addActionListener( new ActionListener()
+		{
+		    public void actionPerformed(ActionEvent e)
+		    {
+		        loadCombatSimulationOne();
+		    }
+		});
+		//sim two 
+		JButton loadSimulationTwoButton = new JButton("Load Simulation Two");	
+		loadSimulationTwoButton.addActionListener( new ActionListener()
+		{
+		    public void actionPerformed(ActionEvent e)
+		    {
+		        loadCombatSimulationTwo();
+		    }
+		});
+		
+		buttons.add(loadSimulationOneButton);
+		buttons.add(loadSimulationTwoButton);
+		
+		render();
+	}
+	
+	private void render(){
+		for(JButton newButton: buttons){
+			add(newButton);
+		}
+		
+		for(JLabel newLabel: labels){
+			add(newLabel);
+		}		
+	}
+	
+	private void resetWindow(){
+		buttons = new ArrayList<JButton>();
+		labels = new ArrayList<JLabel>();
 	}
 }
